ca9b0e0344a0c8e8e2f644fa0b3f2118deca7489
brandonerose <brandon.ed.rose@gmail.com>
2024-09-28 16:00:20 -0400

    dev

--- R/03_transform_DB.R
+++ R/03_transform_DB.R
@@ -1,27 +1,9 @@
 #' @import RosyUtils
 #' @import RosyApp
-#' @title default_instrument_remap
+#' @title default_forms_transformation
 #' @export
-default_instrument_remap <- function(instruments_remap,merge_form_name = "merged"){
-  instruments_remap$instrument_name_remap <- instruments_remap$instrument_name
-  instruments_remap$instrument_label_remap <- instruments_remap$instrument_label
-  instruments_remap <- instruments_remap[order(instruments_remap$repeating),]
-  rep_rows <- which(instruments_remap$repeating)
-  non_rep_rows <- which(!instruments_remap$repeating)
-  reps <- instruments_remap$instrument_name[rep_rows]
-  non_reps <- instruments_remap$instrument_name[non_rep_rows]
-  lnr <- length(non_reps)
-  lr <- length(reps)
-  if(lnr>0){
-    instruments_remap$instrument_name_remap[non_rep_rows] <- merge_form_name
-    instruments_remap$instrument_label_remap[non_rep_rows] <- instruments_remap$instrument_label[which(instruments_remap$instrument_name==merge_form_name)]
-  }
-  return(instruments_remap)
-}
-#' @title default_transformation
-#' @export
-default_transformation <- function(DB){
-  transformation_template <- DB$metadata$forms
+default_forms_transformation <- function(DB){
+  transformation_template <- DB$metadata$forms[,c("instrument_name","instrument_label","repeating")]#longitudinal_fix
   transformation_template <- transformation_template[order(transformation_template$repeating),]
   merge_form_name <- DB$internals$merge_form_name
   transformation_template$instrument_name_remap <- transformation_template$instrument_name
@@ -38,10 +20,10 @@ default_transformation <- function(DB){
   transformation_template$x_first[which(transformation_template$repeating)] <- T
   return(transformation_template)
 }
-#' @title add_transformation_to_DB
+#' @title add_forms_transformation_to_DB
 #' @export
-add_transformation_to_DB <- function(DB,tranformation_df,ask=T){
-  tranformation_df_cols <-c(
+add_forms_transformation_to_DB <- function(DB,forms_tranformation,ask=T){
+  forms_tranformation_cols <-c(
     "instrument_name",
     "instrument_label",
     "repeating",
@@ -52,110 +34,134 @@ add_transformation_to_DB <- function(DB,tranformation_df,ask=T){
     "by.y",
     "x_first"
   )
-  if(any(!names(tranformation_df)%in%tranformation_df_cols)){
-    bullet_in_console("Use `default_transformation(DB)` is an example!")
-    stop("tranformation_df needs the following colnames... ", tranformation_df_cols %>% as_comma_string())
+  if(any(!names(forms_tranformation)%in%forms_tranformation_cols)){
+    bullet_in_console("Use `default_forms_transformation(DB)` is an example!")
+    stop("forms_tranformation needs the following colnames... ", forms_tranformation_cols %>% as_comma_string())
   }
   choice <- T
   if(!is.null(DB$transformation)){
-    if(!identical(DB$transformation,tranformation_df)){
+    if(!identical(DB$transformation,forms_tranformation)){
       if(ask){
         choice <- utils::askYesNo("Do you want to add transformation? (it doesn't match previous transform)")
       }
     }
   }
   #add more checks
-  DB$transformation <- tranformation_df
+  DB$metadata$forms <- DB$transformation$forms <- forms_tranformation
   return(DB)
 }
 #' @title remap_named_df_list
 #' @export
-remap_named_df_list <- function(named_df_list,instruments_remap,merge_form_name = "merged",merge_list = NULL){
-  forms <- unique(instruments_remap$instrument_name_remap)
-  if(merge_form_name %in% forms){
-    OUT[[merge_form_name]] <- merge_multiple(
-      named_data_list = named_df_list,
-      instrument_names = instruments_remap$instrument_name[which(instruments_remap$instrument_name_remap==merge_form_name)]
-    )
-    forms <- forms[which(forms!=merge_form_name)]
+transform_DB <- function(DB){
+  if(DB$internals$is_transformed){
+    bullet_in_console("Already transformed... nothing to do!",bullet_type = "x")
+    return(DB)
   }
-  has_merge_list <- is_something(merge_list)
-  for(TABLE in forms){
-    OUT[[TABLE]] <- named_df_list[[TABLE]]
-    if(has_merge_list){
-      z<- merge_list[[TABLE]]
-      if(is_something(z)){
-        ref <- OUT[[TABLE]]
-        mer <- named_df_list[[z$merge_to]]
-        if(z$merge_to %in% names(OUT)){
-          mer <- OUT[[z$merge_to]]
-        }
-        by.x <- z$by.x
-        by.y <- z$by.y
-        ref_names<-names(ref)
-        mer_names<-names(mer)
-        # new_name <- by.x %>% vec1_not_in_vec2(by.y)
-        new_names <- ref_names %>% vec1_in_vec2(mer_names) %>% vec1_not_in_vec2(by.x)
-        for(new_name in new_names){
-          COL <- which(colnames(mer)==new_name)
-          replace_name <- paste0(new_name,"_merged")
-          a <- mer[,1:COL]
-          a[[replace_name]] <- a[[COL]]
-          b <- mer[,(COL+1):ncol(mer)]
-          mer <- cbind(a,b)
-        }
-        bad_cols <- which(z$by.x!=z$by.y)
-        z$by.x[bad_cols]
-        z$by.y[bad_cols]
-        if(length(bad_cols)>0){
-          for(col in bad_cols){
-            z$by.x[col] <- colnames(ref)[which(colnames(ref)== z$by.x[col])] <- paste0(z$by.y[col],"_merged")
-          }
-        }
-        by.x <- z$by.x
-        by.y <- z$by.y
-        ref_names<-names(ref) %>% vec1_not_in_vec2(
-          by.x %>% vec1_not_in_vec2(by.y)
-        )
-        mer_names<-names(mer)
-        # new_name <- by.x %>% vec1_not_in_vec2(by.y)
-        del_names <- mer_names %>% vec1_in_vec2(ref_names) %>% vec1_not_in_vec2(by.y)
-        mer[,del_names] <- NULL
-        all.x <- T
-        if(is_something(z$all.x)){
-          all.x <- z$all.x
-        }
-        a<- merge(
-          x = ref,
-          y = mer,
-          by.x = by.x,
-          by.y = by.y,
-          all.x = all.x
-        )
-        all_names <- c(ref_names,names(mer)) %>% unique()
-        if(is_something(z$x_first)){
-          if(z$x_first){
-            all_names <- c(by.y %>% vec1_in_vec2(by.x),names(mer) %>% vec1_not_in_vec2(by.y),ref_names) %>% unique()
-          }
-        }
-        a<- a[,match(all_names,names(a))]
-        OUT[[TABLE]] <- a
+  named_df_list <- DB$data
+  transformation <- DB$transformation$forms
+  DB$transformation$original_col_names <- DB$data %>% names() %>% lapply(function(l){
+    DB$data[[l]] %>% colnames()
+  })
+  names(DB$transformation$original_col_names) <- DB$data %>% names()
+  # if(any(!names(transformation)%in%names(DB$data)))stop("must have all DB$data names in transformation")
+  OUT <- NULL
+  for(i in (1:nrow(transformation))){
+    TABLE <- transformation$instrument_name[i]
+    a<- transformation[i,]
+    z<-as.list(a)
+    ref <- named_df_list[[TABLE]]
+    by.x <- z$by.x <- z$by.x %>% strsplit("\\+") %>% unlist()
+    by.y <- z$by.y <- z$by.y %>% strsplit("\\+") %>% unlist()
+    if(length(z$by.x) != length(z$by.y) )stop("by.x and by.y must be same length... [",z$instrument_name,"] (",z$by.x %>% as_comma_string(),") AND (",z$by.y %>% as_comma_string(),")")
+    if(TABLE == z$merge_to){
+      OUT[[z$instrument_name_remap]] <- ref
+    }else{
+      mer <- named_df_list[[z$merge_to]]
+      if(z$merge_to %in% names(OUT)){
+        mer <- OUT[[z$merge_to]]
       }
+      ref_names<-names(ref)
+      mer_names<-names(mer)
+      # new_name <- by.x %>% vec1_not_in_vec2(by.y)
+      new_names <- ref_names %>% vec1_in_vec2(mer_names) %>% vec1_not_in_vec2(by.x)
+      for(new_name in new_names){
+        COL <- which(colnames(mer)==new_name)
+        replace_name <- paste0(new_name,"_merged")
+        a <- mer[,1:COL]
+        a[[replace_name]] <- a[[COL]]
+        b <- mer[,(COL+1):ncol(mer)]
+        mer <- cbind(a,b)
+      }
+      bad_cols <- which(!by.x%in%by.y)
+      z$by.x[bad_cols]
+      z$by.y[bad_cols]
+      if(length(bad_cols)>0){
+        for(col in bad_cols){
+          new_col_name <- paste0(z$by.y[col],"_merged")
+          ref[[new_col_name]] <- ref[[z$by.x[col]]]
+          z$by.x[col] <- new_col_name
+        }
+      }
+      by.x <- z$by.x
+      by.y <- z$by.y
+      ref_names<-names(ref) %>% vec1_not_in_vec2(
+        by.x %>% vec1_not_in_vec2(by.y)
+      )
+      mer_names<-names(mer)
+      # new_name <- by.x %>% vec1_not_in_vec2(by.y)
+      del_names <- mer_names %>% vec1_in_vec2(ref_names) %>% vec1_not_in_vec2(by.y)
+      mer[,del_names] <- NULL
+      all.x <- T
+      if(is_something(z$all.x)){
+        all.x <- z$all.x
+      }
+      a<- merge(
+        x = ref,
+        y = mer,
+        by.x = by.x,
+        by.y = by.y,
+        all.x = all.x
+      )
+      all_names <- c(ref_names,names(mer)) %>% unique()
+      if(is_something(z$x_first)){
+        if(!z$x_first){
+          all_names <- c(by.y %>% vec1_in_vec2(by.x),names(mer) %>% vec1_not_in_vec2(by.y),ref_names) %>% unique()
+        }
+      }
+      a<- a[,match(all_names,names(a))]
+      OUT[[z$instrument_name_remap]] <- a
     }
   }
-  return(OUT)
+  if(any(!names(OUT)%in%unique(transformation$instrument_name_remap)))stop("not all names in OUT objext. Something wrong with transform_DB()")
+  DB$data <- OUT
+  DB$internals$is_transformed <- T
+  bullet_in_console(paste0(DB$short_name," transformed according to `DB$transformation`"),bullet_type = "v")
+  return(DB)
 }
 #' @title purify_names_df_list
 #' @export
-purify_names_df_list <- function(named_df_list,instruments_remap,metadata,merge_list = NULL){
-  if(!is_named_df_list(named_df_list)) stop("`named_df_list` is not a named df list")
-  for(TABLE in names(named_df_list)){
-    original_forms <- instruments_remap$instrument_name[which(instruments_remap$instrument_name_remap==TABLE)]
-    DF <- named_df_list[[TABLE]]
-    COLS <- c(merge_list[[TABLE]][["key"]],metadata$field_name[which(metadata$form_name%in%original_forms)]) %>% unique()
-    COLS<- COLS %>% vec1_in_vec2(colnames(DF))
-    DF <- DF[,COLS]
-    named_df_list[[TABLE]] <- DF
+untransform_DB <- function(DB){
+  if(!DB$internals$is_transformed){
+    bullet_in_console("Already not transformed... nothing to do!",bullet_type = "x")
+    return(DB)
   }
-  return(named_df_list)
+  named_df_list <- DB$data
+  forms_transformation <- DB$transformation$forms
+  original_form_names <- DB$metadata$forms$instrument_name
+  original_fields <- DB$metadata$fields
+  keys <- DB$metadata$form_key_cols
+  OUT <- NULL
+  if(any(!original_form_names%in%forms_transformation$instrument_name))stop("Must have all original form names in transformation!")
+  # TABLE <- original_form_names%>%sample1()
+  for(TABLE in original_form_names){
+    DF <- named_df_list[[forms_transformation$instrument_name_remap[which(forms_transformation$instrument_name==TABLE)]]]
+    COLS <- DB$transformation$original_col_names[[TABLE]]
+    if(any(!COLS%in%colnames(DF)))stop("Missing cols from orginal DF transformation... ",TABLE)
+    DF <- DF[,COLS]
+    OUT[[TABLE]] <- DF
+  }
+  DB$data <- OUT
+  DB$internals$is_transformed <- F
+  bullet_in_console(paste0(DB$short_name," untransformed according to `DB$transformation`"),bullet_type = "v")
+  return(DB)
 }
