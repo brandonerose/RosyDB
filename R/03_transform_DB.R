1bd4eb3f8428d58c02fd1bacc0d834b34510dc00
brandonerose <brandon.ed.rose@gmail.com>
2024-09-30 09:47:02 -0400

    dev

--- R/03_transform_DB.R
+++ R/03_transform_DB.R
@@ -137,11 +137,12 @@ transform_DB <- function(DB){
   DB$data <- OUT
   DB$internals$is_transformed <- T
   DB$transformation$original_forms <- DB$metadata$forms
-  transformation2 <- transformation
-  transformation2$instrument_name <- transformation2$instrument_name_remap
-  transformation2$instrument_label <- transformation2$instrument_label_remap
-  transformation2 <- transformation2[,c("instrument_name","instrument_label","repeating")] %>% unique()
-  DB$metadata$forms <- transformation2
+  DB$transformation$original_fields <- DB$metadata$fields
+  transformation_edit <- transformation
+  transformation_edit$instrument_name <- transformation_edit$instrument_name_remap
+  transformation_edit$instrument_label <- transformation_edit$instrument_label_remap
+  transformation_edit <- transformation_edit[,c("instrument_name","instrument_label","repeating")] %>% unique()
+  DB$metadata$forms <- transformation_edit
   bullet_in_console(paste0(DB$short_name," transformed according to `DB$transformation`"),bullet_type = "v")
   return(DB)
 }
@@ -171,5 +172,65 @@ untransform_DB <- function(DB){
   DB$internals$is_transformed <- F
   DB$metadata$forms <- DB$transformation$original_forms
   bullet_in_console(paste0(DB$short_name," untransformed according to `DB$transformation`"),bullet_type = "v")
+  return(DB)
+}
+#' @title add_edit_fields
+#' @export
+add_edit_fields <- function(
+    DB,
+    field_name,
+    form_name,
+    field_type,
+    field_type_R = NA,
+    field_label = NA,
+    select_choices_or_calculations = NA,
+    field_note = NA,
+    identifier = "",
+    units = NA,
+    insert_after,
+    data_func = NULL
+) {
+  DB <-validate_DB(DB)
+  # if(!DB$data_transform %>% is_something())stop("Must have transformed data to add new vars.")
+  field_row <- data.frame(
+    field_name = field_name,
+    form_name = form_name,
+    field_type = field_type,
+    field_label = field_label,
+    select_choices_or_calculations = choice_vector_string(select_choices_or_calculations),
+    field_note = field_note,
+    identifier = identifier,
+    field_type_R = field_type_R,
+    units = units,
+    in_original_redcap = field_name%in%DB$metadata$fields$field_name,
+    field_label_short = field_label
+  )
+  # if(any(DB$metadata$fields$field_name==field_name))stop("field_name already included")
+  if(missing(insert_after)){
+    i<-which(DB$metadata$fields$form_name == form_name&DB$metadata$fields$field_name == paste0(form_name,"_complete"))
+    if(length(i)>0){
+      if(i[[1]]>1){
+        i <- i-1
+      }
+    }
+    if(length(i)==0){
+      i<-which(DB$metadata$fields$form_name == form_name)
+    }
+    if(length(i)>1){
+      i <- i[[1]]
+    }
+    if(length(i)==0)i <- nrow(DB$metadata$fields$field_name)
+  }else{
+    i<-which(DB$metadata$fields$field_name == insert_after)
+  }
+  if(length(i)==0)stop("insert_after error")
+  top <- DB$metadata$fields[1:i,]
+  bottom <- DB$metadata$fields[(i+1):nrow(DB$metadata$fields),]
+  # DB$metadata$fields <- dplyr::bind_rows(top,field_row) %>% dplyr::bind_rows(bottom)
+  if(is.null(data_func))warning("if no `data_func` is provided, the column is only added to the metadata",immediate. = T)
+  DB$transformation$fields[[field_name]]<-list()
+  DB$transformation$fields[[field_name]]$field_row <- field_row
+  DB$transformation$fields[[field_name]]$field_func <- data_func
+  message("added '",field_name,"' column")
   return(DB)
 }
