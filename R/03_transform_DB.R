6abb989651ead1316192fdacb27d6bbdf000cdf9
brandonerose <brandon.ed.rose@gmail.com>
2024-10-01 15:15:52 -0400

    dev

--- R/03_transform_DB.R
+++ R/03_transform_DB.R
@@ -254,7 +254,7 @@ run_transformation_fields <- function(DB,ask = T){
           message_pass = paste0(form_name," - ",field_name,": ")
         )
         if(is_something(DF)){
-          DB$data_update$transform[[field_name]] <- DF
+          DB$data_update$from_transform[[field_name]] <- DF
         }
       }
     }
