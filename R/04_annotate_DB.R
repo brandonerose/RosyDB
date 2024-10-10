653fef9148bde685e3509c4d388dfa4fdcd0b769
brandonerose <brandon.ed.rose@gmail.com>
2024-10-09 20:09:04 -0400

    dev

--- R/04_annotate_DB.R
+++ R/04_annotate_DB.R
@@ -102,7 +102,7 @@ annotate_choices <- function(DB){
     if(is.null(DF))return(0)
     if(nrow(DF)==0)return(0)
     sum(DF[,choices$field_name[i]]==choices$name[i],na.rm = T)
-    print(i)
+    # print(i)
   }) %>% unlist()
   choices$n_total <- 1:nrow(choices) %>% lapply(function(i){
     DF <- DB$data[[choices$form_name[i]]]
